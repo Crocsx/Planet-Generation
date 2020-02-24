b9b65f07b36677dbb64931bd60f213c88abd6812
Crocsx <crocsx.gaming@gmail.com>
2020-02-24 12:27:10 +0900

    Mountain only appear in continent

--- Assets/Scripts/Planet/ShapeGenerator.cs
+++ Assets/Scripts/Planet/ShapeGenerator.cs
@@ -19,12 +19,23 @@ public class ShapeGenerator
     // Update is called once per frame
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
     {
+        float firstLayerValue = 0;
         float elevation = 0;
-        for (int i = 0; i < noiseFilters.Length; i++)
+
+        if(noiseFilters.Length > 0)
+        {
+            firstLayerValue = noiseFilters[0].Evaluate(pointOnUnitSphere);
+            if(settings.noiseLayers[0].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+        for (int i = 1; i < noiseFilters.Length; i++)
         {
             if (settings.noiseLayers[i].enabled)
             {
-                elevation += noiseFilters[i].Evaluate(pointOnUnitSphere);
+                float mask = (settings.noiseLayers[i].useFirstAsMask) ? firstLayerValue : 1;
+                elevation += noiseFilters[i].Evaluate(pointOnUnitSphere) * mask;
             }
         }
         return pointOnUnitSphere * settings.radius * (1 + elevation);
